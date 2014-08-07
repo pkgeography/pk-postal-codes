71af8fb25a49c8af14b0de0bec98c870627f21f1
Jabran Rafique <hello@jabran.me>
2014-08-07 07:43:30 +0100

    update PDO connection and query execution

--- src/index.php
+++ src/index.php
@@ -6,13 +6,53 @@ $dbhost = DBHOST;
 $dbname = DB;
 $dbuser = DBUSER;
 $dbpassword = DBPASSWORD;
+$pdo = null;
 
 try {
-	// build a database connection
-	$db_handle = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpassword);
-	var_dump($db_handle);
-} catch(PDOException $e) {
-	// output error message
-	echo $e->getMessage();
 
+	// build a database connection
+	$pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpassword);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+
+	// output error message
+	echo 'Error: ' . $e->getMessage();
+}
+
+if ($pdo !== null) {
+
+	// query parameters
+	$params = array(
+		// ':post_office',
+		// ':postal_code',
+		// ':account_office',
+		// ':branch_postal_code',
+		':province' => 'sindh'
+	);
+
+	// setup query
+	$query = sprintf("
+		SELECT 
+			`id`,
+			`post_office`,
+			`postal_code`,
+			`account_office`,
+			`province`,
+			`branch_postal_code` 
+		FROM `%s` 
+		WHERE `province` = %s
+		LIMIT 0, 10
+	", DBTBL, ':province');
+
+	// prepare query
+	$stmt = $pdo->prepare($query);
+
+	// execute query
+	$stmt->execute($params);
+
+	// fetch data
+	$data = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+	
 }
