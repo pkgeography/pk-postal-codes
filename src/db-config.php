399695fc356e725a3571c0b4a41a6fe0ddd11106
Jabran Rafique <hello@jabran.me>
2014-08-07 07:47:51 +0100

    add db template

---
+++ src/db-config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Database configuration
+ */
+
+// Database host
+define("DBHOST", "localhost");
+
+// Database name
+define("DB", "");
+
+// Database username
+define("DBUSER", "");
+
+// Database password
+define("DBPASSWORD", "");
+
+// Table name
+define("DBTBL", "");
